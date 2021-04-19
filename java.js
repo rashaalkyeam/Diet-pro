5e1240cdf39f209d5c1d7493ee18856c85ad7001
RashaAlQayyam <>
2021-04-19 11:45:05 +0000

    add

--- java.js
+++ java.js
@@ -3,20 +3,38 @@ alert('To calculate the calories you need, enter the following information:');
 
 var Length=prompt('Enter your height');
 var Weight=prompt('Enter your Weight');
-if(length>=160&& Weight>=50)
+if(length<=160&& Weight<=50)
 {
   alert('Your daily calorie needs 1686');
-}else if (length>=170&& Weight>=60)
+}else if (length<=170&& Weight<=60)
 {
 alert('Your daily calorie needs 2420');
 
-}else if (length>=180&& Weight>=70)
+}else if (length<=180&& Weight<=70)
 {
 alert('Your daily calorie needs 2516');
 
 }else{
   alert('Please follow a diet')
 }
-function colories(length,Weight){
-  
+var i=prompt('Enter your number of exercises you do');
+while(i>0&&i<10){
+  alert('Doing a good job');
+  var img='<img src="imag/e.jfif" alt="sport">';
+  document.write(img)
+  i++;
 }
+  prompt('Enter your number of exercises you do;water sport, Climbing sport, Walking sport');
+  if (sporttype=='water sport')
+  {
+     alert('Nice sport');
+  } else if(sporttype=='Climbing sport'){
+    alert('An amazing sport');
+
+  }else if(sporttype=='Walking sport'){
+    alert('An enjoyable sport');
+
+  }else{
+    alert('If allowed, enter the required sport.');
+  }
+
