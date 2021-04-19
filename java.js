12f4155caf18d092064978b39c91e49a56974daa
RashaAlQayyam <>
2021-04-19 11:06:37 +0000

    add

--- java.js
+++ java.js
@@ -3,19 +3,20 @@ alert('To calculate the calories you need, enter the following information:');
 
 var Length=prompt('Enter your height');
 var Weight=prompt('Enter your Weight');
-
 if(length>=160&& Weight>=50)
 {
-
   alert('Your daily calorie needs 1686');
-} if else(length>=170&& Weight>=60)
+}else if (length>=170&& Weight>=60)
 {
 alert('Your daily calorie needs 2420');
 
-}if else(length>=180&& Weight>=70)
+}else if (length>=180&& Weight>=70)
 {
 alert('Your daily calorie needs 2516');
 
 }else{
   alert('Please follow a diet')
+}
+function colories(length,Weight){
+  
 }
